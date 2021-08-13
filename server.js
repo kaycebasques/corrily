fd5b2744ce72e8ea2aa8dd7b5507fc86b1948c9d
Glitch (glitch-hello-node) <none>
2021-08-13 04:13:44 +0000

    🐌📅 Checkpoint ./server.js:111231/1300 ./package.json:111231/1236

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const API_KEY = 'b82fd0fa-2a1b-4226-b6c0-22884ae97f84';
+const axios = require('axios');
 
-app.get()
+app.get('/', (request, response) => {
+  response.send('JAYCE');
+});
+
+app.get('/test', async (request, response) => {
+  let result = await axios({
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      api_key: API_KEY
+    },
+    data: {
+      products: 'monthly',
+      country: 'BR',
+      integrations: [
+        'stripe'
+      ]
+    },
+    'https://mainapi-staging-4hqypo5h6a-uc.a.run.app/v1/prices'
+  });
+});
 
 app.listen(8080, () => {
-  console.log('Listening!');
+  console.log(process.API_KEY);
 });
