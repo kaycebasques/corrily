02066d75ec979175a3adfefb822228a9f980ee8c
Glitch (glitch-hello-node) <none>
2021-08-13 16:28:19 +0000

    ❄️💕 Checkpoint ./static/index.html:111231/472 ./static/index.js:111231/461

--- static/index.js
+++ static/index.js
@@ -5,7 +5,8 @@
       `/api/price?ip=${ipData.query}&products=annual,monthly&country=${ipData.countryCode}`;
   const priceResponse = await fetch(priceUrl);
   const priceData = await priceResponse.json();
-  const displayPrice = 
-      `${priceData.currency_symbol}${priceData.products.monthly.price} ${priceData.currency}`;
-  document.querySelector('#price').textContent = displayPrice;
+  document.querySelector('#monthly').textContent =
+      `Monthly: ${priceData.currency_symbol}${priceData.products.monthly.price} ${priceData.currency}`;
+  document.querySelector('#annual').textContent =
+      `Annual: ${priceData.currency_symbol}${priceData.products.annual.price} ${priceData.currency}`;
 })();
