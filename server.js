d467f34adeb61bb5d59926c954f4e3b45f08fef3
Glitch (glitch-hello-node) <none>
2021-08-14 04:41:45 +0000

    🚧🌸 Checkpoint ./static/index.css:111231/87 ./server.js:111231/995

--- server.js
+++ server.js
@@ -14,16 +14,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('static'));
 
 app.post('/subscribe', async (request, response) => {
+  const ip = request.body.ip;
+  const productId = request.body.interval === 'month' ? 'monthly' : 'year';
   const session = await stripe.checkout.sessions.create({
     mode: 'subscription',
     payment_method_types: ['card'],
     line_items: [
       {
-        price: request.body.id,
         quantity: 1,
         price_data: {
-          unit_amount: request.body.price,
-          currency: request.body.currency,
+          product: request.body.id,
+          unit_amount: sessionData[ip].products[productId].price,
+          currency: sessionData[ip].currency,
           recurring: {
             interval: request.body.interval
           }
@@ -60,27 +62,6 @@ app.get('/', async (request, response) => {
   };
   response.send(nunjucks.render('index.html', renderData));
 });
-
-// app.get('/api/price', async (request, response) => {
-//   const {ip, country} = request.query;
-//   const products = request.query.products.split(',');
-//   const result = await axios({
-//     method: 'post',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       api_key: process.env.CORRILY
-//     },
-//     data: {
-//       ip,
-//       products,
-//       country
-//     },
-//     // TODO: Hardcode to https://client.corrily.com/v1/prices when finished
-//     url: process.env.URL
-//   });
-//   data[ip] = result.data;
-//   response.json(result.data);
-// });
 
 app.listen(8080, () => {
   console.info('App is running');
